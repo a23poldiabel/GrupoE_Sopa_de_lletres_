fbc4580e58181c1a7356871a8fe89639b156d383
a23aletrigut <a23aletrigut@inspedralbes.cat>
2024-01-25 10:21:49 +0100

    4 tests palabraValida

--- test/Test_palabraValida.java
+++ test/Test_palabraValida.java
@@ -10,7 +10,19 @@ class Test_palabraValida {
 
     @org.junit.jupiter.api.Test
     void palabraValida_OK_6_lletres() {
-        String paraula = "Aleska";
+        String paraula = "chased";
         assertTrue(PrincipalCodigoSopa.palabraValida(paraula));
+    }
+
+    @org.junit.jupiter.api.Test
+    void palabraValida_isEmpty() {
+        String paraula = " ";
+        assertFalse(PrincipalCodigoSopa.palabraValida(paraula));
+    }
+
+    @org.junit.jupiter.api.Test
+    void palabraValida_isUpperCase() {
+        String paraula = "HOLA";
+        assertTrue(PrincipalCodigoSopa.palabraValida(paraula.toUpperCase()));
     }
 }
