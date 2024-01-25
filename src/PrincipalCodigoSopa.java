e9e1a5d763a4045fbd756a19dc0c97c0b08ba216
a21xavmarvel <a21xavmarvel@inspedralbes.cat>
2024-01-25 13:26:10 +0100

    xavier

--- src/PrincipalCodigoSopa.java
+++ src/PrincipalCodigoSopa.java
@@ -7,7 +7,7 @@ public class PrincipalCodigoSopa {
         //String operadorSopaDeLetras = fraseValida(input);
         char[][] matrizDeSopa = new char[10][10];
         boolean[][] matrizDeSopaRojo = new boolean[10][10];
-        //stringDividida(operadorSopaDeLetras, matrizDeSopa);
+        stringDividida(operadorSopaDeLetras, matrizDeSopa);
         int casos = 0;
         //printearMatrizFinal(matrizDeSopa, matrizDeSopaRojo);
         while (casos < 5) {
@@ -47,4 +47,25 @@ public class PrincipalCodigoSopa {
         }
     }
 
+
+    /**
+     * Fills a two-dimensional character array with elements from a string.
+     *
+     * @param operadorSopaDeLetras The string from which characters will be taken to fill the array.
+     * @param matrizDeSopa The two-dimensional array to be filled with characters.
+     * @return The two-dimensional array filled with characters from the string.
+     *         If the length of the string is less than the size of the array, the string is looped.
+     *         If the string is longer than the size of the array, additional characters are ignored.
+     */
+
+    public static char[][] stringDividida (String operadorSopaDeLetras, char[][] matrizDeSopa) {
+        int index =0;
+        for (int f = 0; f< 10; f++) {
+            System.out.println();
+            for (int c = 0; c < 10; c++) {
+                matrizDeSopa [f][c] = operadorSopaDeLetras.charAt(index++);
+            }
+        }
+        return matrizDeSopa;
+    }
 }
