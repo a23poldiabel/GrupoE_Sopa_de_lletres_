684cda248613d7a89a73dc75c014e853a7f51da9
a22arnmaljoa <a22arnmaljoa@inspedralbes.cat>
2024-01-25 11:38:57 +0100

    fraseValida

--- src/PrincipalCodigoSopa.java
+++ src/PrincipalCodigoSopa.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PrincipalCodigoSopa {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //String operadorSopaDeLetras = fraseValida(input);
+        String operadorSopaDeLetras = fraseValida(input);
         char[][] matrizDeSopa = new char[10][10];
         boolean[][] matrizDeSopaRojo = new boolean[10][10];
         //stringDividida(operadorSopaDeLetras, matrizDeSopa);
@@ -32,6 +32,40 @@ public class PrincipalCodigoSopa {
             }
         }
     }
+
+    /**
+     * fraseValida: It may seem false at first glance, but the necessary conditions are applied to make it 'true'.
+     * In the 'while' loop, a System.out.println() is used to input the word puzzle phrase, and 'frase' is understood as the next input.
+     * In the 'if' statement, 'frase' is interpreted in the 'validarFrase' function, and the specified conditions are applied in its instance (100 characters and only A-Z). At this point, 'fraseValida = true'.
+     * In the 'else' statement, 'fraseValida' is still understood as 'false,' and the loop is repeated once again.
+     */
+    public static String fraseValida(Scanner input) {
+        boolean fraseValida = false;
+        String frase = "";
+        while (!fraseValida) {
+            System.out.println("Ingresa la frase para crear la sopa de letras:");
+            frase = input.nextLine();
+            if (validarFrase(frase)) {
+                fraseValida = true;
+                System.out.println("La frase cumple las condiciones.");
+            } else {
+                System.out.println("La frase no cumple las condiciones, intentalo de nuevo.");
+            }
+        }
+        return frase;
+    }
+
+
+    /**
+     * Users are required to write a 100-character word under various conditions.
+     * They need to be 100 exactly characters.
+     * The input must consist solely of letters (uppercase) from 'A' to 'Z', numbers and special characters are not allowed.
+     */
+    private static boolean validarFrase(String frase) {
+        return frase.length() == 100 && frase.matches("^[A-Z]*$");
+    }
+
+
     /**
      * With this function we are verifying if the input is valid.
      * We will use an if loop to verify if the word length is less or the same as 10 characters
