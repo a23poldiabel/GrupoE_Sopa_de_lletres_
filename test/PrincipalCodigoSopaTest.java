8dbf4c77b5d2b5e954309609de5e2400c916e68d
a21xavmarvel <a21xavmarvel@gmail.com>
2024-01-26 11:37:35 +0100

    teststringDividida

--- test/PrincipalCodigoSopaTest.java
+++ test/PrincipalCodigoSopaTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrincipalCodigoSopaTest {
@@ -9,5 +11,9 @@ class PrincipalCodigoSopaTest {
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void stringDividida() {
     }
 }
